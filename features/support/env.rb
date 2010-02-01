793bc7962a4d2ccdb0d5db398209eb00ac77e6aa
Jose Peleteiro <jose@peleteiro.net>
2010-02-01 10:22:07 -0200

    Trying to make tests running for runcoderun.com

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,7 +22,7 @@ end
 Spec::Matchers.define :be_same_image_as do |expected_filename|
   match do |actual_blob|
     actual = Magick::Image.from_blob(actual_blob).first
-    expected = Magick::Image.read("features/support/files/#{expected_filename}").first
+    expected = Magick::Image.read(File.expand_path(__FILE__ + "/../files/#{expected_filename}")).first
     actual.get_pixels(0, 0, actual.columns, actual.rows) == expected.get_pixels(0, 0, expected.columns, expected.rows)
   end
 end
