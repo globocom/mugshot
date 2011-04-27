077292dda174ca3f955e3208d87d8e3f37fe6cda
Jose Peleteiro <jose@peleteiro.net>
2011-04-27 15:17:10 -0300

    New FSStorege path strategy

--- lib/mugshot/fs_storage.rb
+++ lib/mugshot/fs_storage.rb
@@ -2,19 +2,17 @@
 require 'fileutils'
 class Mugshot::FSStorage < Mugshot::Storage
   def write(bin)
-    asset_id.tap do |id|
-      @old_id = String.new(id)
-      path = define_path(id)
-      FileUtils.mkdir_p(File.join(@root_path, path.split("/")[0..6].join("/")))
-      File.open(File.join(@root_path, path), "w") do |fw|
-        fw.write(bin)
-      end
+    id = asset_id
+    path = id_to_path(id)
+    FileUtils.mkdir_p(File.dirname(File.join(@root_path, path)))
+    File.open(File.join(@root_path, path), "w") do |fw|
+      fw.write(bin)
     end
-    @old_id
+    id
   end
 
   def read(id)
-    path = define_path(id)
+    path = id_to_path(id)
     file = File.join(@root_path, path)
     return nil unless File.exist? file
     Mugshot::Image.new File.open(file)
@@ -27,14 +25,10 @@ class Mugshot::FSStorage < Mugshot::Storage
     FileUtils.mkdir_p(root_path)
   end
 
-  private
-
-  def define_path(id)
-    path = String.new(id)
-    begin
-      7.times { |i| path = path.insert(3 + (i * 4), "/") }
-    rescue IndexError => index_error
-      p index_error
+  def id_to_path(id)
+    path = id.to_s.clone
+    4.times do |i|
+      path = path.insert(3 + (i * 3) + i, '/')
     end
     path
   end
--- spec/mugshot/fs_storage_spec.rb
+++ spec/mugshot/fs_storage_spec.rb
@@ -8,7 +8,7 @@ describe Mugshot::FSStorage do
 
   after :each do
     require 'fileutils'
-    FileUtils.rm_rf("/tmp/mugshot/spec")
+    #FileUtils.rm_rf("/tmp/mugshot/spec")
   end
 
   it "should write an image to the filesystem and read it back" do
@@ -18,12 +18,16 @@ describe Mugshot::FSStorage do
     Mugshot::Image.stub!(:new).and_return(image)
 
     id = @fs.write(bin)
-    
+
     image2 = @fs.read(id)
     image2.should == image
   end
 
   it "should return nil when an image with the given id doesn't exist on the filesystem" do
     @fs.read('nonexistant-id').should be_nil
+  end
+
+  it "should convert an id to path with 4r levels of directories" do
+    @fs.send(:id_to_path, "a9657a30c7df012c736512313b021ce1").should == "a96/57a/30c/7df/012c736512313b021ce1"
   end
 end
